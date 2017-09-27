1be8d794a714a443618282a86f2cc5d2d028ca70
Long Hei Tam <etam1@Longs-MBP.hsd1.ca.comcast.net>
2017-09-26 23:54:23 -0700

    forgot to add test

---
+++ clientTest.py
@@ -0,0 +1,42 @@
+import unittest
+import client
+import json
+
+class ClientTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = client.APPLICATION.test_client()
+
+    def testGetAcceptAppJson(self):
+        rv = self.app.get('/',headers={'Accept': 'application/json'})
+        self.assertEqual(json.loads(rv.data), {'message': 'Good Morning'})
+        self.assertEqual(rv.status_code, 200)
+
+    def testGetNoAccept(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.data, '<p>Hello World</p>')
+        self.assertEqual(rv.status_code, 200)
+    
+    def testGetNegativeCase(self):
+        rv = self.app.get('/',headers={'Accept': 'application/yml'})
+        self.assertEqual(json.loads(rv.data), {'message': 'Only accept application/json or no accept header'})
+        self.assertEqual(rv.status_code, 400)
+
+    def testPostAcceptAppJson(self):
+        rv = self.app.post('/',headers={'Accept': 'application/json'})
+        self.assertEqual(json.loads(rv.data), {'message': 'Good Morning'})
+        self.assertEqual(rv.status_code, 200)
+
+    def testPostNoAccept(self):
+        rv = self.app.post('/')
+        self.assertEqual(rv.data, '<p>Hello World</p>')
+        self.assertEqual(rv.status_code, 200)
+
+    def testPostNegativeCase(self):
+        rv = self.app.post('/',headers={'Accept': 'application/yml'})
+        self.assertEqual(json.loads(rv.data), {'message': 'Only accept application/json or no accept header'})
+        self.assertEqual(rv.status_code, 400)
+
+if __name__ == '__main__':
+    unittest.main()
+
