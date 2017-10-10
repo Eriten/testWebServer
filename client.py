e6a2a8153c3f406934dbc733c5ea056fa9d7cd30
Long Hei Tam <etam1@Longs-MacBook-Pro.local>
2017-10-10 15:31:15 -0700

    Added language support and behaviour of no accept header endpoint

--- client.py
+++ client.py
@@ -1,19 +1,32 @@
 from flask import Flask, request, make_response, jsonify
+import json
 
 def handle_json_request():
     return jsonify({
         'message': 'Good Morning'
     })
 
-def handle_no_accept_request():
-    return '<p>Hello World</p>'
+def handle_no_accept_request(name, helloString):
+    return "<p>" + helloString + " " + name + "</p>"
+
+def read_value_by_locale(lang, value):
+    # 1. We should handle the case where lang.json doesn't exist
+    # 2. Could we override lang.json
+    with open('lang.json') as data_file:
+        # Handling of invalid json
+        lang_file_data = json.load(data_file)
+        return lang_file_data[lang][value]
 
 APPLICATION = Flask(__name__)
 @APPLICATION.route('/', methods=['POST', 'GET'])
 def single_endpoint():
     accept_headers = request.headers.get('accept')
+    parameters = request.args
+    # Language is universal to all endpoints, preloading
+    lang = parameters.get('lang', 'en')
     if not accept_headers or '*/*' in accept_headers:
-        return handle_no_accept_request()
+        name = parameters.get('name', 'World')
+        return handle_no_accept_request(name, read_value_by_locale(lang, 'Hello'))
     if 'application/json' in accept_headers:
         return handle_json_request()
     return jsonify({'message': 'Only accept application/json or no accept header'}), 400
--- clientTest.py
+++ clientTest.py
@@ -16,6 +16,11 @@ class ClientTestCase(unittest.TestCase):
         rv = self.app.get('/')
         self.assertEqual(rv.data, '<p>Hello World</p>')
         self.assertEqual(rv.status_code, 200)
+
+    def testGetNoAcceptWithNameParmaeter(self):
+        rv = self.app.get('/?name=Eric')
+        self.assertEqual(rv.data, '<p>Hello Eric</p>')
+        self.assertEqual(rv.status_code, 200)
     
     def testGetNegativeCase(self):
         rv = self.app.get('/',headers={'Accept': 'application/yml'})
@@ -36,6 +41,11 @@ class ClientTestCase(unittest.TestCase):
         rv = self.app.post('/',headers={'Accept': 'application/yml'})
         self.assertEqual(json.loads(rv.data), {'message': 'Only accept application/json or no accept header'})
         self.assertEqual(rv.status_code, 400)
+
+    def testPostNoAcceptWithNameParmaeter(self):
+        rv = self.app.post('/?name=Eric')
+        self.assertEqual(rv.data, '<p>Hello Eric</p>')
+        self.assertEqual(rv.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
